d35a8bcab1573cf4de393a6701b64a8a3fbc826f
yuuuko86326274 <yuko.67.53@gmail.com>
2020-08-10 19:35:33 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "messages#index"
+  root "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
   end
